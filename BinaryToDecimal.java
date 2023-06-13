9154cdc21d752146bf48f11fba41c0187ddc2dac
Ma. Jensen Nicole Dela Rosa <jensennics.dlrs@gmail.com>
2023-06-14 00:21:40 +0800

    Program 3 completed. Implemented for loop to validate the binary number and added if else condition to convert the descimal value of binary.

--- BinaryToDecimal.java
+++ BinaryToDecimal.java
@@ -5,17 +5,34 @@ public class BinaryToDecimal {
     Scanner scanner = new Scanner(System.in);
     
     // Prompt the user to enter the value of N
-    System.out.print("Enter the number of binary numbers: ");
+    System.out.print("\nEnter the number of binary numbers: ");
     int numBinaryNumbers = scanner.nextInt();
     
     // Process each binary number and convert it to decimal
     for (int i = 0; i < numBinaryNumbers; i++) {
 
         // Prompt the user to enter a binary number
-        System.out.print("Enter binary number " + (i + 1) + ": ");
+        System.out.print("\nEnter binary number " + (i + 1) + ": ");
         String binary = scanner.next();
+
+        // Validate the binary number
+        boolean valid = true;
+        for (char digit : binary.toCharArray()) {
+            if (digit != '0' && digit != '1') {
+                valid = false;
+                break;
+            }
+        } 
+
+        // Convert the binary number to decimal or display an error message
+        if (valid) {
+            int decimal = Integer.parseInt(binary, 2);
+            System.out.println("Decimal representation: " + decimal);
+        } else {
+            System.out.println("Invalid binary number: " + binary);
+        }
     }
-    
-    scanner.close();
+
+        scanner.close();
     }
 }
