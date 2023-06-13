f808a756f2b7e363a5e544b6bf813d822f741151
Ma. Jensen Nicole Dela Rosa <jensennics.dlrs@gmail.com>
2023-06-13 22:39:28 +0800

    Added user input and loop for checking balanced parentheses

---
+++ BalanceParenthesis.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BalanceParenthesis {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of sets of parentheses: ");
+        int numberOfSets = scanner.nextInt(); // Number of sets of parentheses
+        
+        for (int i = 0; i < numberOfSets; i++) {
+            String parentheses = scanner.next(); // Read each set of parentheses
+            
+            // Implement if else conditions
+        }
+        
+        scanner.close();
+    }
+}
