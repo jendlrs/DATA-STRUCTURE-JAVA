6fec768848bceb821b97577399af7b2fd89fdfd1
Ma. Jensen Nicole Dela Rosa <jensennics.dlrs@gmail.com>
2023-06-13 22:49:22 +0800

    Added prompt to get input from the user for each set of parentheses

--- BalanceParenthesis.java
+++ BalanceParenthesis.java
@@ -7,6 +7,7 @@ public class BalanceParenthesis {
         int numberOfSets = scanner.nextInt(); // Number of sets of parentheses
         
         for (int i = 0; i < numberOfSets; i++) {
+            System.out.print("Enter the parentheses set " + (i + 1) + ": "); //Prompt for each set of parentheses
             String parentheses = scanner.next(); // Read each set of parentheses
             
             // Implement if else conditions
