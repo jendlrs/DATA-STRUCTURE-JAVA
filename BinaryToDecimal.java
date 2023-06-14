4e9975b08d45b4219eb2daa796a81d3434753558
Ma. Jensen Nicole Dela Rosa <jensennics.dlrs@gmail.com>
2023-06-14 17:07:52 +0800

    Refactored code. The system gets user output one time and display the output also one time thru stack.

--- BinaryToDecimal.java
+++ BinaryToDecimal.java
@@ -2,37 +2,41 @@ import java.util.Scanner;
 
 public class BinaryToDecimal {
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    
-    // Prompt the user to enter the value of N
-    System.out.print("\nEnter the number of binary numbers: ");
-    int numBinaryNumbers = scanner.nextInt();
-    
-    // Process each binary number and convert it to decimal
-    for (int i = 0; i < numBinaryNumbers; i++) {
+        Scanner scanner = new Scanner(System.in);
 
-        // Prompt the user to enter a binary number
-        System.out.print("\nEnter binary number " + (i + 1) + ": ");
-        String binary = scanner.next();
+        // Prompt the user to enter the number of binary numbers
+        System.out.print("Enter the number of binary numbers: ");
+        int numBinaryNumbers = scanner.nextInt();
+        int[] binaryArray = new int[numBinaryNumbers];
 
-        // Validate the binary number
-        boolean valid = true;
-        for (char digit : binary.toCharArray()) {
-            if (digit != '0' && digit != '1') {
-                valid = false;
-                break;
-            }
-        } 
+        // Process each binary number and convert it to decimal
+        for (int i = 0; i < numBinaryNumbers; i++) {
+            // Prompt the user to enter a binary number
+            System.out.print("Enter binary number " + (i + 1) + ": ");
+            int binary = scanner.nextInt();
+            binaryArray[i] = binary;
+        }
 
-        // Convert the binary number to decimal or display an error message
-        if (valid) {
-            int decimal = Integer.parseInt(binary, 2);
-            System.out.println("Decimal representation: " + decimal);
-        } else {
-            System.out.println("Invalid binary number: " + binary);
+        scanner.close();
+
+        //convert binary numbers to decimal
+        int[] decimalResultArray = binaryToDecimal(binaryArray);
+
+        for (int i = 0; i < decimalResultArray.length; i++) {
+            int decimal = decimalResultArray[i];
+            System.out.println("Decimal number " + (i + 1) + ": " + decimal);
         }
     }
 
-        scanner.close();
+    //convert an array of binary numbers to decimal
+    public static int[] binaryToDecimal(int[] binaryArray) {
+        int[] decimalResultArray = new int[binaryArray.length];
+
+        for (int i = 0; i < binaryArray.length; i++) {
+            String binaryString = String.valueOf(binaryArray[i]);
+            decimalResultArray[i] = Integer.parseInt(binaryString, 2);
+        }
+
+        return decimalResultArray;
     }
 }
